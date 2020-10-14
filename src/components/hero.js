1d54d6feefaac1eb6629c0c1d4a95dbcbbbb1d00
Chris Courtney <c2@Mr-Green.local>
2020-10-14 10:27:32 -0500

    update homepage

--- src/components/hero.js
+++ src/components/hero.js
@@ -9,7 +9,7 @@ const Hero = ({ siteTitle }) => (
         <div className="hero-text">
             <h1 className="hero-headline"><span className="lightWeight">The</span>Circuit</h1>
             <h2>Decoding <br/>the Cook County <br/>Court System</h2>
-            <h3>The Circuit is a multi-yearlong collaborative journalism project focused on investigative and revealing the inner workings of the Cook County Circuit Court system.</h3>
+            <h3>The Circuit is a multi-yearlong collaborative journalism project focused on investigating and revealing the inner workings of the Cook County Circuit Court system.</h3>
         </div>
         <div className="hero-img">
         </div>
