6f3a263c818964a6162911c60d7bd3aa4215582d
Chris Courtney <c2@Mr-Green.local>
2020-10-19 09:34:47 -0500

    nav tweaks

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,12 +6,14 @@ import "./styles.css"
 
 const Hero = ({ siteTitle }) => (
     <section className="hero">
-        <div className="hero-text">
-            <h1 className="hero-headline"><span className="lightWeight">The</span>Circuit</h1>
-            <h2>Decoding <br/>the Cook County <br/>Court System</h2>
-            <h3>The Circuit is a multi-yearlong collaborative journalism project focused on investigating and revealing the inner workings of the Cook County Circuit Court system.</h3>
-        </div>
-        <div className="hero-img">
+        <div className="hero-wrap">
+            <div className="hero-text">
+                <h1 className="hero-headline"><span className="lightWeight">The</span>Circuit</h1>
+                <h2>Decoding <br/>the Cook County <br/>Court System</h2>
+                <h3>The Circuit is a multi-yearlong collaborative journalism project focused on investigating and revealing the inner workings of the Cook County Circuit Court system.</h3>
+            </div>
+            <div className="hero-img">
+            </div>
         </div>
     </section>
 )
