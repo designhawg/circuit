20fe800ff3f37b784d80bdb54864ab7707368e82
Chris Courtney <c2@Mr-Green.local>
2020-10-20 16:58:45 -0500

    footer update

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,6 +4,7 @@ import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
 import Hero from "./hero"
+import Footer from "./footer"
 
 import "./styles.css"
 
@@ -31,35 +32,7 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
       </div>
-      <footer>
-        <div className="footer-contain">
-          <div className="footer-left">
-            <h5 className="label">AN INVESTIGATIVE JOURNALISM COLLABORATION</h5>
-            <div className="footerOutlets">
-              <a href="https://www.bettergov.org/" target="_blank"> 
-                <img title="Better Government Association" alt="Better Government Association" src={require('../images/BGA.png')}/>
-              </a>
-              <a href="https://www.chicagoreporter.com/" target="_blank"> 
-                <img title="Chicago Reporter" alt="Chicago Reporer" src={require('../images/CR.png')}/>
-              </a>
-              <a href="https://www.injusticewatch.org/" target="_blank"> 
-                <img title="Injustice Watch" alt="Injustice Watch" src={require('../images/IW.png')}/>
-              </a>
-              <a href="https://datamade.us/" target="_blank"> 
-                <img title="Datamade" alt="Datamade" src={require('../images/DM.png')}/>
-              </a>
-              <a href="https://harris.uchicago.edu/" target="_blank"> 
-                <img title="Harris Public Policy" alt="Harris Public Policy" src={require('../images/HPP.png')}/>
-              </a>
-            </div>
-            <p>Copyright 2020</p>
-          </div>
-          <div className="footer-right">
-            <img alt="The Circuit logo" src={require('../images/circuit_block.png')} />
-
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   )
 }
