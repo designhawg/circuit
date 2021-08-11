63a593fc52e4076f71e72c8bf006e16ae623bef9
Sam McAlilly <sammcalilly@gmail.com>
2021-08-11 15:51:54 -0500

    update footer

--- src/components/footer_ES.js
+++ src/components/footer_ES.js
@@ -13,20 +13,14 @@ const Footer = () => (
                     <a href="https://www.bettergov.org/" target="_blank"> 
                         <img title="Better Government Association" alt="Better Government Association" src={require('../images/BGA.png')}/>
                     </a>
-                    <a href="https://www.chicagoreporter.com/" target="_blank"> 
-                        <img title="Chicago Reporter" alt="Chicago Reporer" src={require('../images/CR.png')}/>
-                    </a>
                     <a href="https://www.injusticewatch.org/" target="_blank"> 
                         <img title="Injustice Watch" alt="Injustice Watch" src={require('../images/IW.png')}/>
                     </a>
                     <a href="https://datamade.us/" target="_blank"> 
                         <img title="Datamade" alt="Datamade" src={require('../images/DM.png')}/>
                     </a>
-                    <a href="https://harris.uchicago.edu/" target="_blank"> 
-                        <img title="Harris Public Policy" alt="Harris Public Policy" src={require('../images/HPP.png')}/>
-                    </a>
                 </div>
-                <p>Copyright 2020</p>
+                <p>Copyright 2021</p>
             </div>
             <div className="footer-right">
                 <img alt="The Circuit logo" src={require('../images/circuit_block.png')} />
